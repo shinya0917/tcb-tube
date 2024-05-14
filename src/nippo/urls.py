19f0a75d5abc149b0d389b27ee4961d14baf11d4
moja0917 <dirmojamoja@gmail.com>
2024-05-14 23:18:28 +0900

    html作成

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
- 
+from .views import nippoListView, nippoDetailView
+
 urlpatterns = [
-  
+    path("", nippoListView),
+    path("detail/<int:number>", nippoDetailView),
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from random import randint
 
-# Create your views here.
+def nippoListView(request):
+	return render(request, "nippo/nippo-list.html")
+
+
+def nippoDetailView(request, number):
+    template_name="nippo/nippo-detail.html"
+    random_int = randint(1,10)
+    ctx = {
+        "random_number": random_int,
+        "number": number,
+    }
+    return render(request, template_name, ctx)
